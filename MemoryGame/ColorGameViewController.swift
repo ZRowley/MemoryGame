1e9c24d6cfe9fa521b457de5be4e4b6299328c30
Lauren Carder <lcarder@laurens-macbook-air-9.local>
2019-04-15 11:39:12 -0500

    Some other stuff

---
+++ MemoryGame/ColorGameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorGameViewController.swift
+//  MemoryGame
+//
+//  Created by Lauren Carder on 4/15/19.
+//  Copyright © 2019 Zach Rowley . All rights reserved.
+//
+
+import UIKit
+
+class ColorGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MemoryGame/InitialViewController.swift
+++ MemoryGame/InitialViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var memoryGameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+    
 
 
 }
