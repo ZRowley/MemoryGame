595202ae1f2a58eb5f9ef6fa8ad0a039d65eac54
Lauren Carder <lcarder@laurens-macbook-air-9.local>
2019-05-03 11:45:44 -0500

    Timer Added

--- MemoryGame/MemoryGameViewController.swift
+++ MemoryGame/MemoryGameViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class MemoryGameViewController: UIViewController {
+    
+    @IBOutlet weak var box1: UIView!
+    @IBOutlet weak var box2: UIView!
+    @IBOutlet weak var box3: UIView!
+    @IBOutlet weak var box4: UIView!
+    @IBOutlet weak var box5: UIView!
+    @IBOutlet weak var box6: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +23,6 @@ class MemoryGameViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
